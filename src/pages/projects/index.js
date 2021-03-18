ee2f10089cbac563d503ffe316ac599239ec9122
avicrayyy <dsgbdomingo@gmail.com>
2021-03-18 13:01:56 +0800

    Added Projects Pages Template

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -4,7 +4,29 @@ import Layout from "../../components/Layout"
 export default function Projects() {
   return (
     <Layout>
-      <div>Projects</div>
+      <div className="projects-containter" style={{ flexDirection: "column" }}>
+        <div>Projects</div>
+        <div className="project-item">
+          <p>01</p>
+          <p>Project Title</p>
+          <p>Project Description</p>
+        </div>
+        <div className="project-item">
+          <p>01</p>
+          <p>Project Title</p>
+          <p>Project Description</p>
+        </div>
+        <div className="project-item">
+          <p>01</p>
+          <p>Project Title</p>
+          <p>Project Description</p>
+        </div>
+        <div className="project-item">
+          <p>01</p>
+          <p>Project Title</p>
+          <p>Project Description</p>
+        </div>
+      </div>
     </Layout>
   )
 }
